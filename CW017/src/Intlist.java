59b05b5841a4e3309644702b124ffddb6008afeb
Danil Apsadikov <DonPonchik18@gmail.com>
2018-12-03 19:48:56 +0300

    Homework added

---
+++ CW017/src/Intlist.java
@@ -0,0 +1,21 @@
+public interface Intlist {
+    void add(int var1);
+
+    void add(int var1, int var2);
+
+    int get(int var1);
+
+    int size();
+
+    boolean contains(int var1);
+
+    void remove(int var1);
+
+    void sort();
+
+    int[] toArray();
+
+    void addAll(Intlist var1);
+
+    void addAll(Intlist var1, int var2);
+}
